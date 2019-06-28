0ed8305d553e9d0e6d72b52cb421aeae4c5d69ae
Rashard Pleasant <rashard.pleasant@gmail.com>
2019-06-28 01:49:36 +0000

    Automatically backed up by Learn

---
+++ greeting.rb
@@ -0,0 +1,17 @@
+def greeting
+  puts "Hello World"
+end
+$ ruby greeting.rb
+
+def greeting
+  puts "Hello World"
+end
+
+greeting
+
+$ ruby greeting.rb
+Hello World
+$
+def one_plus_one
+  1+1
+end
